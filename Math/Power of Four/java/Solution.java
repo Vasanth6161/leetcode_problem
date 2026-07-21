34aec3bc5612254c983a9c5baa59d89008a01b46
Vasanth6161 <vasanthragupathi6164@gmail.com>
2026-07-21 13:35:55 +0530

    feat: Add Power of Four

--- Math/Power of Four/java/Solution.java
+++ Math/Power of Four/java/Solution.java
@@ -1,8 +1,11 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if (n <= 0) 
+        if (n <= 0) {
             return false;
         }
-        return Math.log(n) / Math.log(4) % 1 == 0;        
+        while(n%4==0){
+            n=n/4;
+        }
+        return n==1;
     }
 }
